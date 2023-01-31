a372ffc5e279597adb766c741271065b65a2ab6b
Citizen Four <danielfetrujillo@gmail.com>
2023-01-31 16:59:38 -0500

    installed react95

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from "react";
+import { Button, ThemeProvider } from "@react95/core";
+import "@react95/icons/icons.css";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <ThemeProvider>
+        <Button>Click me!</Button>
+      </ThemeProvider>
+    </div>
+  );
 };
 
 export default App;
